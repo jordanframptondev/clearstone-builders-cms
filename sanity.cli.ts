3036c3bc613c8ae854d9d7cc4a35c5387728ee3b
jordan-frampton <jordan.frampton@flybreeze.com>
2024-10-16 19:36:38 -0600

    adds sanity deployment config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -10,4 +10,10 @@ export default defineCliConfig({
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  /**
+   * The name of the studio host.
+   * Learn more at https://www.sanity.io/docs/cli#studio-host
+   * Studio deployed to https://clearstone-builders.sanity.studio/
+   */
+  studioHost: 'clearstone-builders'
 })
